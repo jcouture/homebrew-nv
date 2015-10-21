d2fbbc253cd6e7ee59d054f4c410111f64441b50
Jean-Philippe Couture <jcouture@gmail.com>
2015-10-20 20:34:45 -0400

    Release v0.0.2.

--- nv.rb
+++ nv.rb
@@ -1,8 +1,8 @@
 class Nv < Formula
   desc "Lightweight utility to load context specific environment variables"
   homepage "https://github.com/jcouture/nv"
-  url "https://github.com/jcouture/nv/archive/0.0.1.tar.gz"
-  sha256 "b48d7fe9d1651fdb5f16e05147d1dc4b70337f0abcd43dfc5e0a89fa97e5a9b7"
+  url "https://github.com/jcouture/nv/archive/0.0.2.tar.gz"
+  sha256 "0f68ac10657e776c2271a2f699354b4627ca231a6f2e4464eed5a36dac439dc9"
 
   head "https://github.com/jcouture/nv.git"
 
